3d84c538c95dbdac27dc3018e4fc52628f10682e
yuansui <yuansui08@gmail.com>
2022-07-08 09:38:20 +0800

    modify run_bert_classification.py

--- bertology/evaluation/run_bert_classification.py
+++ bertology/evaluation/run_bert_classification.py
@@ -30,7 +30,7 @@ def save_model(model, dirpath):
 class BertForClassification(nn.Module):
     def __init__(self):
         super(BertForClassification, self).__init__()
-        self.backbone = AutoModel.from_pretrained("../../module/bert-base-uncased")
+        self.backbone = AutoModel.from_pretrained("distilbert-base-uncased")
         for p in self.parameters():
             p.requires_grad = False  # freeze the backbone model
         self.linear1 = nn.Linear(768, 256)
@@ -89,7 +89,7 @@ def main():
         type=str,
         help="Where do you want to store the pre-trained models downloaded from s3",
     )
-    parser.add_argument("--batch_size", default=4, type=int, help="Batch size.")
+    parser.add_argument("--batch_size", default=8, type=int, help="Batch size.")
     parser.add_argument("--no_shuffle", action="store_true", help="Whether not to shuffle the dataloader")
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--no_cuda", action="store_true", help="Whether not to use CUDA when available")
@@ -121,65 +121,58 @@ def main():
     optimizer = torch.optim.Adam(
         filter(lambda p: p.requires_grad,
                model.parameters()))  # only update the parameters who are set to requires_grad
-    # PATH = "../../output/bert_classification"
-    output_model_file = args.output_dir + ".bin"
 
-    if os.path.exists(output_model_file):
-        # make sure the model_file is not empty
-        model.load_state_dict(torch.load(output_model_file))
-    else:
-        print("\nThe model file is empty, train the model!\n")
-        # add tqdm to the pipe
-        for _ in tqdm(range(1, args.epochs + 1)):
-            # train the model
-            model.train()
-            for batch in tqdm(train_dataloader):
-                data = list(batch[0])
-                targets = torch.tensor(list(batch[1])).to(args.device)
-                optimizer.zero_grad()
-                # Style-1:
-                encoding = tokenizer.batch_encode_plus(data, return_tensors='pt', padding=True, truncation=True,
-                                                       max_length=args.max_length,
-                                                       add_special_tokens=True)
-                input_ids = encoding['input_ids'].to(args.device)
-                attention_mask = encoding['attention_mask'].to(args.device)
+    print("\nThe model file is empty, train the model!\n")
+    # add tqdm to the pipe
+    for _ in tqdm(range(1, args.epochs + 1)):
+        # train the model
+        model.train()
+        for batch in tqdm(train_dataloader):
+            data = list(batch[0])
+            targets = torch.tensor(list(batch[1])).to(args.device)
+            optimizer.zero_grad()
+            # Style-1:
+            encoding = tokenizer.batch_encode_plus(data, return_tensors='pt', padding=True, truncation=True,
+                                                   max_length=args.max_length,
+                                                   add_special_tokens=True)
+            input_ids = encoding['input_ids'].to(args.device)
+            attention_mask = encoding['attention_mask'].to(args.device)
 
-                outputs = model(input_ids, attention_mask)
-                print(outputs)
-                logits = outputs[:, -1]
-                predictions = torch.softmax(logits, dim=-1)
+            outputs = model(input_ids, attention_mask)
+            logits = outputs[:, -1]
+            predictions = torch.softmax(logits, dim=-1)
 
-                # Style-2:
-                # model_input = tokenizer(data, padding="max_length", max_length=args.max_length, truncation=True,
-                #                         return_tensors="pt")
-                # model_input.to(args.device)
-                # outputs = model(model_input["input_ids"], model_input["attention_mask"])
-                # logits = outputs
-                # predictions = torch.softmax(logits, dim=-1)
+            # Style-2:
+            # model_input = tokenizer(data, padding="max_length", max_length=args.max_length, truncation=True,
+            #                         return_tensors="pt")
+            # model_input.to(args.device)
+            # outputs = model(model_input["input_ids"], model_input["attention_mask"])
+            # logits = outputs
+            # predictions = torch.softmax(logits, dim=-1)
 
-                loss = criterion(predictions,
-                                 targets)  # make sure the predictions and the targets are on the same device!
-                loss.backward()
-                optimizer.step()
-            # evaluate the model (no need to use the without_grad():)
-            model.eval()
-            glue_metric = datasets.load_metric('glue', 'sst2')  # load the metrics
-            for batch in tqdm(eval_dataloader):
-                data = list(batch[0])
-                targets = torch.tensor(list(batch[1])).to(args.device)
-                model_input = tokenizer(data, padding="max_length", max_length=args.max_length, truncation=True,
-                                        return_tensors="pt")
-                model_input.to(args.device)
-                outputs = model(model_input["input_ids"], model_input["attention_mask"])
-                logits = outputs
-                predictions = torch.squeeze(torch.softmax(logits, dim=-1)).sum(0)
-                model_predictions = []
-                for i in range(len(targets)):
-                    model_predictions.append(0) if predictions[i][0] > predictions[i][1] else model_predictions.append(
-                        1)
-                glue_metric.add_batch(predictions=model_predictions, references=targets)
-            final_score = glue_metric.compute()
-            print(final_score)
+            loss = criterion(predictions,
+                             targets)  # make sure the predictions and the targets are on the same device!
+            loss.backward()
+            optimizer.step()
+    # evaluate the model (no need to use the without_grad():)
+    model.eval()
+    glue_metric = datasets.load_metric('glue', 'sst2')  # load the metrics
+    for batch in tqdm(eval_dataloader):
+        data = list(batch[0])
+        targets = torch.tensor(list(batch[1])).to(args.device)
+        model_input = tokenizer(data, padding="max_length", max_length=args.max_length, truncation=True,
+                                return_tensors="pt")
+        model_input.to(args.device)
+        outputs = model(model_input["input_ids"], model_input["attention_mask"])
+        logits = outputs
+        predictions = torch.squeeze(torch.softmax(logits, dim=-1)).sum(0)
+        model_predictions = []
+        for i in range(len(targets)):
+            model_predictions.append(0) if predictions[i][0] > predictions[i][1] else model_predictions.append(
+                1)
+        glue_metric.add_batch(predictions=model_predictions, references=targets)
+    final_score = glue_metric.compute()
+    print(final_score)
 
         # torch.save(model.state_dict(), args.output_dir + ".bin")  # save the model
 
