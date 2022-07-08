02bcd616c3659d51153c455232ffc5d341a37982
yuansui <yuansui08@gmail.com>
2022-07-09 00:11:32 +0800

    add the demo run_bert_classification.py

--- bertology/evaluation/run_bert_classification.py
+++ bertology/evaluation/run_bert_classification.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoModel, AutoTokenizer
+from transformers import BertForTokenClassification, BertTokenizer
 
 from dataset.sst2.sst2 import SST2
 
@@ -31,8 +32,6 @@ class BertForClassification(nn.Module):
     def __init__(self):
         super(BertForClassification, self).__init__()
         self.backbone = AutoModel.from_pretrained("distilbert-base-uncased")
-        for p in self.parameters():
-            p.requires_grad = False  # freeze the backbone model
         self.linear1 = nn.Linear(768, 256)
         self.dropout = nn.Dropout(0.5)
         self.linear2 = nn.Linear(256, 2)  # 2 is the number of classes in this example
@@ -49,79 +48,35 @@ class BertForClassification(nn.Module):
 def main():
     parser = argparse.ArgumentParser()
     # Required parameters
-    parser.add_argument(
-        "--dataset",
-        default="SST2",
-        required=True,
-        type=str,
-        help="The dataset name, the options can be sst, SST2, etc"
-    )
-    parser.add_argument(
-        "--model_name_or_path",
-        default="../../module/bert-base-uncased",
-        type=str,
-        required=True,
-        help="Path to save the pretrained model"
-    )
-    parser.add_argument(
-        "--output_dir",
-        default="../../output/bert_classification",
-        type=str,
-        required=True,
-        help="The output directory where the model predictions and checkpoints will be written."
-    )
+    parser.add_argument("--dataset",default="SST2",type=str,help="The dataset name, the options can be sst, SST2, etc")
+    parser.add_argument("--model_name_or_path",default="bert-base-uncased",type=str,help="Path to save the pretrained model")
+    parser.add_argument("--output_dir",default="../../output/bert_classification",type=str,help="The output directory where the model predictions and checkpoints will be written.")
     # Options parameters
-    parser.add_argument(
-        "--config_name",
-        default="",
-        type=str,
-        help="Pretrained config name or path if not the same as model_name_or_path",
-    )
-    parser.add_argument(
-        "--tokenizer_name",
-        default="",
-        type=str,
-        help="Pretrained tokenizer name or path if not the same as model_name_or_path",
-    )
-    parser.add_argument(
-        "--cache_dir",
-        default=None,
-        type=str,
-        help="Where do you want to store the pre-trained models downloaded from s3",
-    )
+    parser.add_argument("--config_name",default="",type=str,help="Pretrained config name or path if not the same as model_name_or_path",)
+    parser.add_argument("--tokenizer_name",default="",type=str,help="Pretrained tokenizer name or path if not the same as model_name_or_path",)
+    parser.add_argument("--cache_dir",default=None,type=str,help="Where do you want to store the pre-trained models downloaded from s3",)
     parser.add_argument("--batch_size", default=8, type=int, help="Batch size.")
     parser.add_argument("--no_shuffle", action="store_true", help="Whether not to shuffle the dataloader")
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--no_cuda", action="store_true", help="Whether not to use CUDA when available")
-    parser.add_argument("--epochs", default=3, type=int)
+    parser.add_argument("--epochs", default=15, type=int)
     parser.add_argument("--max_length", default=50, type=int, help="Max length of the tokenization")
     args = parser.parse_args()
 
     # Setup devices (No distributed training here)
     args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
-    # Print/save training arguments
-    # os.makedirs(args.output_dir, exist_ok=True)
-    # torch.save(args, os.path.join(args.output_dir, "run_args.bin"))
 
     # Prepare dataset
     training_data, evaluation_data = [], []
-    if args.dataset == "SST2":
-        training_data = SST2("train")
-        evaluation_data = SST2("valid")
-    elif args.dataset == "sst":  # will add later
-        pass
-    else:
-        print("The dataset is empty!")
+    training_data = SST2("train")
+    evaluation_data = SST2("valid")
     train_dataloader = DataLoader(training_data, batch_size=args.batch_size, shuffle=args.no_shuffle)
     eval_dataloader = DataLoader(evaluation_data, batch_size=args.batch_size, shuffle=args.no_shuffle)
     # Reload the model
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)  # "../../module/bert-base-uncased"
-    model = BertForClassification().to(args.device)
+    tokenizer = BertTokenizer.from_pretrained(args.model_name_or_path)
+    model = BertForTokenClassification.from_pretrained(args.model_name_or_path).to(args.device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(
-        filter(lambda p: p.requires_grad,
-               model.parameters()))  # only update the parameters who are set to requires_grad
-
+    optimizer = torch.optim.Adam(model.parameters())
     print("\nThe model file is empty, train the model!\n")
     # add tqdm to the pipe
     for _ in tqdm(range(1, args.epochs + 1)):
@@ -129,63 +84,39 @@ def main():
         model.train()
         for batch in tqdm(train_dataloader):
             data = list(batch[0])
-            targets = torch.tensor(list(batch[1])).to(args.device)
+            targets = torch.tensor(list(batch[1])).float().to(args.device)
+            # print(targets)
             optimizer.zero_grad()
-            # Style-1:
-            encoding = tokenizer.batch_encode_plus(data, return_tensors='pt', padding=True, truncation=True,
-                                                   max_length=args.max_length,
-                                                   add_special_tokens=True)
-            input_ids = encoding['input_ids'].to(args.device)
-            attention_mask = encoding['attention_mask'].to(args.device)
+            model_input = tokenizer(data, padding="max_length", max_length=args.max_length, truncation=True,
+                                    return_tensors="pt")
+            input_ids = model_input['input_ids'].to(args.device)
+            attention_mask = model_input['attention_mask'].to(args.device)
 
             outputs = model(input_ids, attention_mask)
-            logits = outputs[:, -1]
-            predictions = torch.softmax(logits, dim=-1)
-
-            # Style-2:
-            # model_input = tokenizer(data, padding="max_length", max_length=args.max_length, truncation=True,
-            #                         return_tensors="pt")
-            # model_input.to(args.device)
-            # outputs = model(model_input["input_ids"], model_input["attention_mask"])
-            # logits = outputs
-            # predictions = torch.softmax(logits, dim=-1)
-
-            loss = criterion(predictions,
-                             targets)  # make sure the predictions and the targets are on the same device!
+            logits = outputs.logits[:,0,:] # CLS
+            # print(logits)
+            predictions = torch.argmax(logits, dim=-1).float()
+            # print(predictions)
+            loss = criterion(predictions, targets)  # make sure the predictions and the targets are on the same device!
+            loss.requires_grad = True
             loss.backward()
+            print(loss)
             optimizer.step()
     # evaluate the model (no need to use the without_grad():)
     model.eval()
     glue_metric = datasets.load_metric('glue', 'sst2')  # load the metrics
     for batch in tqdm(eval_dataloader):
         data = list(batch[0])
-        targets = torch.tensor(list(batch[1])).to(args.device)
+        targets = torch.tensor(list(batch[1])).float().to(args.device)
         model_input = tokenizer(data, padding="max_length", max_length=args.max_length, truncation=True,
                                 return_tensors="pt")
         model_input.to(args.device)
         outputs = model(model_input["input_ids"], model_input["attention_mask"])
-        logits = outputs
-        predictions = torch.squeeze(torch.softmax(logits, dim=-1)).sum(0)
-        model_predictions = []
-        for i in range(len(targets)):
-            model_predictions.append(0) if predictions[i][0] > predictions[i][1] else model_predictions.append(
-                1)
-        glue_metric.add_batch(predictions=model_predictions, references=targets)
+        logits = outputs.logits[:,0,:] # CLS
+        predictions = torch.argmax(logits, dim=-1).float()
+        glue_metric.add_batch(predictions=predictions, references=targets)
     final_score = glue_metric.compute()
     print(final_score)
-
-        # torch.save(model.state_dict(), args.output_dir + ".bin")  # save the model
-
-    # test the model
-    input = "Pretty much sucks, but has a funny moment or two"
-    model_input = tokenizer(input, padding="max_length", max_length=args.max_length, truncation=True,
-                            return_tensors="pt")
-    model_input.to(args.device)
-    outputs = model(model_input["input_ids"], model_input["attention_mask"])
-    logits = outputs
-    predictions = torch.squeeze(torch.softmax(logits, dim=-1)).sum(0)
-    labels = "negative" if predictions[0] > predictions[1] else "positive"
-    print(labels)
 
 
 if __name__ == "__main__":
