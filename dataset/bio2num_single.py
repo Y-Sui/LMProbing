e26b5c45192598a0caccd0c1c8e20291fdf3713f
v-yuansui <v-yuansui@microsoft.com>
2022-08-30 19:04:46 +0800

    fix bugs in bio2num.py

--- dataset/bio2num_single.py
+++ dataset/bio2num_single.py
@@ -2,6 +2,7 @@ import os
 import json
 import random
 import re
+import os
 
 import pandas as pd
 
@@ -11,9 +12,13 @@ def get_files_path(filePath):
     return the file list
     """
     raw_files = os.listdir(filePath)
+    file_paths = []
     for i in range(len(raw_files)):
-        raw_files[i] = filePath + "/" + raw_files[i]
-    return raw_files
+        if raw_files[i].find("train") == -1 and raw_files[i].find("eval") == -1:
+           file_paths.append(filePath + "/" + raw_files[i])
+        # else:
+        #     raw_files.pop(i)  # remove the elements
+    return file_paths
 
 
 def get_pure_tokens(tokenList):
@@ -22,14 +27,14 @@ def get_pure_tokens(tokenList):
     """
     tokens = []
     for i in range(len(tokenList)):
-        tokenList[i] = tokenList[i].\
-            replace("O-", "").replace("X-", "").replace("B-LOC-", "").replace("B-MISC-", "").\
-            replace("B-ORG-","").replace("B-PER-", "").replace("B-ADJP-","").replace("B-ADVP-", "").\
-            replace("B-CONJP-", "").replace("B-INTJ-", "").replace("B-LST-", "").replace("B-NP-", "").\
-            replace("B-PP-","").replace("B-PRT-", "").replace("B-SBAR-", "").replace("B-VP-", ""). \
-            replace("I-LOC-", "").replace("I-MISC-", "").replace("I-ORG-", "").replace("I-PER-", "").\
-            replace("I-ADJP-","").replace("I-ADVP-", "").replace("I-CONJP-", "").replace("I-INTJ-", "").\
-            replace("I-LST-", "").replace("I-NP-", "").replace("I-PP-", "").replace("I-PRT-","").\
+        tokenList[i] = tokenList[i]. \
+            replace("O-", "").replace("X-", "").replace("B-LOC-", "").replace("B-MISC-", ""). \
+            replace("B-ORG-", "").replace("B-PER-", "").replace("B-ADJP-", "").replace("B-ADVP-", ""). \
+            replace("B-CONJP-", "").replace("B-INTJ-", "").replace("B-LST-", "").replace("B-NP-", ""). \
+            replace("B-PP-", "").replace("B-PRT-", "").replace("B-SBAR-", "").replace("B-VP-", ""). \
+            replace("I-LOC-", "").replace("I-MISC-", "").replace("I-ORG-", "").replace("I-PER-", ""). \
+            replace("I-ADJP-", "").replace("I-ADVP-", "").replace("I-CONJP-", "").replace("I-INTJ-", ""). \
+            replace("I-LST-", "").replace("I-NP-", "").replace("I-PP-", "").replace("I-PRT-", ""). \
             replace("I-SBAR-", "").replace("I-VP-", "")
         tokens.append(tokenList[i].split())
     return tokens
@@ -40,24 +45,26 @@ def get_tags(tagList, flag="ner"):
     generate the mixed tag list
     """
     if flag == "ner":
-        tag_dict = {"O":0,"B-ORG":1,"I-ORG":2,"B-PER":3,"I-PER":4,"B-MISC":5,"I-MISC":6,"B-LOC":7,"I-LOC":8}
+        tag_dict = {"O": 0, "B-ORG": 1, "I-ORG": 2, "B-PER": 3, "I-PER": 4, "B-MISC": 5, "I-MISC": 6, "B-LOC": 7,
+                    "I-LOC": 8}
     if flag == "chunk":
-        tag_dict = {"O":0,"B-ADJP":1,"I-ADJP":2,"B-ADVP":3,"I-ADVP":4,"B-CONJP":5,"I-CONJP":6,"B-INTJ":7,"I-INTJ":8,
-                      "B-LST":9, "I-LST":10, "B-NP":11, "I-NP":12, "B-PP":13, "I-PP":14, "B-PRT":15, "I-PRT":16,
-                      "B-SBAR":17, "I-SBAR":18, "B-VP":19, "I-VP":20}
+        tag_dict = {"O": 0, "B-ADJP": 1, "I-ADJP": 2, "B-ADVP": 3, "I-ADVP": 4, "B-CONJP": 5, "I-CONJP": 6, "B-INTJ": 7,
+                    "I-INTJ": 8,
+                    "B-LST": 9, "I-LST": 10, "B-NP": 11, "I-NP": 12, "B-PP": 13, "I-PP": 14, "B-PRT": 15, "I-PRT": 16,
+                    "B-SBAR": 17, "I-SBAR": 18, "B-VP": 19, "I-VP": 20}
     tag_keys = list(tag_dict.keys())
     for i in range(len(tagList)):
         tagList[i] = tagList[i].split()
         for j in range(len(tagList[i])):
             for index in range(len(tag_keys)):
-                if tagList[i][j].__contains__(f"{tag_keys[index]+'-'}"):
+                if tagList[i][j].__contains__(f"{tag_keys[index] + '-'}"):
                     tagList[i][j] = tag_dict[f"{tag_keys[index]}"]
                     break
     # how to deal with X
     for i in range(len(tagList)):
         for j in range(len(tagList[i])):
             if isinstance(tagList[i][j], str) and tagList[i][j].__contains__("X-"):
-                tagList[i][j] = tagList[i][j-1]
+                tagList[i][j] = tagList[i][j - 1]
     return tagList
 
 
@@ -66,6 +73,8 @@ def get_single_tags(tagList, category="LOC"):
     generate the single tag list
     """
     tag_dict = {"O": 0, f"B-{category}": 1, f"I-{category}": 2}
+    if category.islower():
+        tag_dict = {"O": 0, "B-head": 1, "B-dependent": 2}
     tag_keys = list(tag_dict.keys())
     for i in range(len(tagList)):
         tagList[i] = tagList[i].split()
@@ -98,23 +107,24 @@ def data_split(full_list, ratio, shuffle=False):
 
 
 def main():
-    for n in ["ner", "chunk"]:
-        raw_files = get_files_path(f"./{n}")  # obtain the ner files
+    for n in ["ner", "chunk", "pos-fixed", "dependency-fixed"]:
+        print(f"Start to generate {n} datasets...")
+        raw_files = get_files_path(f"./{n}")  # obtain the {ner} files list
         random_files = []
         for file in range(len(raw_files)):
             pd_reader = pd.read_csv(raw_files[file], header=None)[1].tolist()
             token_list = pd_reader.copy()
             tag_list = pd_reader.copy()
             tokens = get_pure_tokens(token_list)  # get the pure tokens
-            flag = "ner" if "ner" in raw_files[file] else "chunk"
-            category = "".join(re.findall(r'[A-Z]', raw_files[
-                file]))  # get the Uppercase file name which refers to the categories of the labels
+            # flag = "ner" if "ner" in raw_files[file] else "chunk"
+            # category = "".join(re.findall(r'[A-Z]', raw_files[file]))  # get the Uppercase file name which refers to the categories of the labels
+            category = raw_files[file].split("_")[-1].replace(".csv", "")
             tags = get_single_tags(tag_list, category)  # get the tags
 
             # save json file
             samples = []
             for i in range(len(tokens)):
-                samples.append({'tokens': tokens[i], f"{flag}_tags": tags[i]})
+                samples.append({'tokens': tokens[i], f"{n}_tags": tags[i]})
 
             # down-sampling
             train_samples, eval_samples = data_split(samples, 0.7, shuffle=True)
@@ -127,7 +137,7 @@ def main():
                 json.dump(train_samples, fp)
             with open(f"{json_file}_eval.json", 'w') as fp:
                 json.dump(eval_samples, fp)
-
+            print(f"The {n} dataset corresponding single files have been saved...")
             # save the mixed tags
             random_files.extend(samples)
 
@@ -138,9 +148,8 @@ def main():
             json.dump(train_files, fp)
         with open(f"{n}_eval.json", "w") as fp:
             json.dump(eval_files, fp)
+        print(f"The {n} dataset corresponding mixed files have been saved...")
+
 
 if __name__ == "__main__":
     main()
-
-
-
