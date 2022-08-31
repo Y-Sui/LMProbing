4c7ac425f132706a5c7176201fa922317e6ff415
v-yuansui <v-yuansui@microsoft.com>
2022-08-31 11:39:43 +0800

    re-run exps

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-python run.py --task "ner" --model_name_or_path "bert-base-uncased" --epochs 15 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
+# python run.py --task "ner" --model_name_or_path "bert-base-uncased" --epochs 15 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
 
-python run.py --task "chunk" --model_name_or_path "bert-base-uncased" --epochs 15 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
+python run.py --task "chunk" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
 
-python run.py --task "dependency-fixed" --model_name_or_path "bert-base-uncased" --epochs 15 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
+python run.py --task "dependency-fixed" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
 
-python run.py --task "pos-fixed" --model_name_or_path "bert-base-uncased" --epochs 15 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
+python run.py --task "pos-fixed" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234
 
