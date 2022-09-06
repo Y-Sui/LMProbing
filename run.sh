2a3912af0c7bbd3758d8a04bf84af36d4aa1e0e6
v-yuansui <v-yuansui@microsoft.com>
2022-09-06 19:07:10 +0800

    test exp

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python run.py --task "ner" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
+python run.py --task "ner" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python run.py --task "chunk" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
+python run.py --task "chunk" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python run.py --task "dependency-fixed" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
+python run.py --task "dependency-fixed" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python run.py --task "pos-fixed" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
+python run.py --task "pos-fixed" --model_name_or_path "bert-base-uncased" --epochs 3 --batch_size 8 --max_length 50 --lr 0.0001 --seed 1234 --profile
 
